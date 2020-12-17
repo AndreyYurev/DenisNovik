1ec4423cf39c55352a635712c6069aa8c81fba72
AndreyYurev <65871877+AndreyYurev@users.noreply.github.com>
2020-12-18 05:13:27 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+
+let hamburger = document.querySelector('.nav-button');
+let menu = document.querySelector('.header__nav');
+let link = document.querySelector('.header__link');
+let body = document.body || document.getElementsByTagName('body')[0];
+
+const toggleMenu = () => {
+	menu.classList.toggle('header__nav--open');
+	hamburger.classList.toggle('nav-button--close');
+	body.classList.toggle('overflow-hidden');
+}
+hamburger.addEventListener('mouseup', e => {
+	e.stopPropagation();
+	toggleMenu();
+});
+document.addEventListener('mouseup', e => {
+	let target = e.target;
+	let its_menu = target == menu;
+	let its_hamburger = target == hamburger;
+	let menu_is_active = menu.classList.contains('header__nav--open');
+
+	if (!its_menu && !its_hamburger && menu_is_active) {
+		toggleMenu();
+
+	};
+})
+
